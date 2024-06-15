7656faf7d5c015a20fb99ba53b222f1e9cd78ab2
Otunla Timilehin <69770115+Duade10@users.noreply.github.com>
2024-06-15 10:20:51 +0100

    Added Pagination To Suppliers Items

--- suppliers/views.py
+++ suppliers/views.py
@@ -1,7 +1,25 @@
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
+from .models import Supplier
+from .serializers import SupplierSerializer
 from rest_framework import viewsets
-from . import models, serializers
+from inventoryitems.serializers import InventoryItemSerializer
 
 
 class SupplierViewSet(viewsets.ModelViewSet):
-    queryset = models.Supplier.objects.all()
-    serializer_class = serializers.SupplierSerializer
+    queryset = Supplier.objects.all()
+    serializer_class = SupplierSerializer
+
+    @action(detail=True, methods=['get'])
+    def items(self, request, pk=None):
+        supplier = self.get_object()
+        items = supplier.items.all()
+
+        paginator = PageNumberPagination()
+        paginator.page_size = 10
+        paginated_items = paginator.paginate_queryset(items, request)
+
+        serializer = InventoryItemSerializer(paginated_items, many=True)
+
+        return paginator.get_paginated_response(serializer.data)
